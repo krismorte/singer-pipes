b97363d4b74ab9003ed4d34260220f00f251b0e6
krisnamourt filho <krisnamourt_ti@hotmail.com>
2021-05-01 10:34:06 -0300

    rm comments

--- target-postgres/gitlab/setup.py
+++ target-postgres/gitlab/setup.py
@@ -24,35 +24,3 @@ if len(records) == 0:
 cur.close()
 conn.commit()
 conn.close()
-
-
-gl_token = os.environ['GL_TOKEN']
-
-headers = {
-    'Authorization': 'Bearer '+ gl_token
-}
-
-
-# request = requests.get(f'https://gitlab.com/api/v4/groups?visibility=private',headers=headers)
-
-# allRepos = ""
-
-# json = request.json()
-# for i in range(0,len(json)):
-#   print(i+1, "Project Name: ",json[i]['name'])
-#   allRepos = allRepos + json[i]['full_path']+" "
-  
-
-# fin = open("gitlab.conf.json", "rt")
-# #read file contents to string
-# data = fin.read()
-# #replace all occurrences of the required string
-# data = data.replace('GL_GROUPS', allRepos)
-# #close the input file
-# fin.close()
-# #open the input file in write mode
-# fin = open("gitlab.conf.json", "wt")
-# #overrite the input file with the resulting data
-# fin.write(data)
-# #close the file
-# fin.close()  
